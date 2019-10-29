930e7737ebd1b0c6db645cacbaa1c1bc52a15e9a
Nguyen Phuong Thuy <phuongthuya3k52@gmail.com>
2019-10-30 00:40:57 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 <?php
 require("dbconnector.php");
 $id = $_POST['productid'];
-$sql = "DELETE FROM product WHERE productid = '$id'";
+$sql = "DELETE FROM product WHERE idToy = '$id'";
 pg_query($conn,$sql); 
 header("Location: /home.php");
 ?>
