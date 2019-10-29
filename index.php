c36124e0b8812ed9e634383255b483fda510d49d
Nguyen Phuong Thuy <phuongthuya3k52@gmail.com>
2019-10-30 00:40:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 					<span class="login100-form-title p-b-33">
 						Login
 					</span>
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz" name="username">
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required" name="username">
 						<input class="input100" type="text" name="username" placeholder="Email">
 						<span class="focus-input100-1"></span>
 						<span class="focus-input100-2"></span>
