6a75480e65212fd273670b5ce31758bcc67656d0
Nguyen Phuong Thuy <phuongthuya3k52@gmail.com>
2019-10-29 14:41:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -159,10 +159,10 @@ body {
 						$resultSet = $stmt->fetchAll();
 						
 
-						for ($i=0; $i<count($resultSet);$i++)
+						foreach($resultSet as $row)
 						{
 
-							echo "<h4>".$resultSet["catname"]."</h4>";		
+							echo "<h4>".$row["catname"]."</h4>";		
 
 						}
 					?>		
@@ -177,9 +177,7 @@ body {
   </div>
   
   <div class="column middle">
-    <h2>Main Content</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+    
   </div>
   
   <div class="column side">
