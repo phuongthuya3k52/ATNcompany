31a0fdd33ad892ecec1dae9761312579b0195f40
Nguyen Phuong Thuy <phuongthuya3k52@gmail.com>
2019-10-29 23:38:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,283 +1,75 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>ATN</title>
-<!-- 	<link rel="stylesheet" href="./style.php">   -->
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-	<!-- Popper JS -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-
-	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<style>
-* {
-  box-sizing: border-box;
-}
-
-body {
-  margin: 0;
-}
-
-/* Style the header */
-.header {
-  background-color: #f1f1f1;
-  padding: 20px;
-  text-align: center;
-}
-
-/* Style the top navigation bar */
-.topnav {
-  overflow: hidden;
-  background-color: #333;
-}
-
-/* Style the topnav links */
-.topnav a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-/* Change color on hover */
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  padding: 12px 16px;
-  z-index: 1;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
-/* Create three unequal columns that floats next to each other */
-.column {
-  float: left;
-  padding: 10px;
-}
-
-/* Left and right column */
-.column.side {
-  width: 20%;
-  background-color: green;
-}
-
-/* Middle column */
-.column.middle {
-  width: 60%;
-  overflow: auto;
-  float: left;
-  height: 1170px;
-  background-color:#FFD3DF;
-
-}
-
-
-.product{
-	background-color:#FFD3DF;
-	height: 345px;
-	width: 28%;
-	margin: 1%;
-	float:left;
-}
-.name{
-	background-color: #D4A5FC;
-	font-size: 20px;
-	font-weight: bold;
-	text-align: center;
-	vertical-align: middle;
-	vertical-align: middle;
-	height:42.5px;
-	}
-.image{
-	background-color:#FFFFFF;
-	height:225px;
-	width: 100%;
-	}
-.price{
-	background-color: #D4A5FC;
-	font-size: 22px;
-	font-weight: bold;
-	text-align: center;
-	vertical-align: middle;
-	height:41.5px;
-	}
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
-@media screen and (max-width: 600px) {
-  .column.side, .column.middle {
-    width: 100%;
-  }
-}
-
-/* Style the footer */
-.footer {
-  background-color: #f1f1f1;
-  padding: 10px;
-  text-align: center;
-}
-</style>
-	
+	<title>Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
-	<?php 
-	//Refer to database
-		$db = parse_url(getenv("DATABASE_URL"));
-
-		$pdo = new PDO("pgsql:" . sprintf(
-   			"host=%s;port=%s;user=%s;password=%s;dbname=%s",
-   			$db["host"],
-    		$db["port"],
-  			$db["user"],
-    		$db["pass"],
-    		ltrim($db["path"], "/")
-		));
-	?>
-	<div class="header">
-  		<table width="1239px">
-			<tr>
-				<td><img src="https://seekvectorlogo.net/wp-content/uploads/2018/08/atn-asian-radio-vector-logo.png" alt="" width="160px" height="160px"></a></td>
-				<td style= "font-weight:bold; font-size:50px; text-align: center;">
-					Welcome to ATN
-				</td>
-				<td style="text-align: right; vertical-align: bottom; padding: 20px" >
-					<form action="search.php" method="GET">
-					<div style="font-size: 24px; color: red; font-weight: bold;">Hotline to order: <br>024 36320669 or 024 36320669</div> <br>
-					<input type="text" name="search"> <input type="submit" value="Search">
-					
-					</form>
-				 </td>
-			</tr>
-		</table>
-
-	</div>
-	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-	  <!-- Links -->
-	  <ul class="navbar-nav">
-	    <li class="nav-item">
-	      <a class="nav-link" href="#">Home</a>
-	    </li>
-	    <li class="nav-item">
-	      <a class="nav-link" href="#">Contact</a>
-	    </li>
-
-	    <!-- Dropdown -->
-	    <li class="nav-item dropdown">
-	      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-	        Product
-	      </a>
-	      <div class="dropdown-menu">
-	      			<?php
-						$sql = "select catname from Category";
-
-						//////////////////////////////////////
-						$stmt = $pdo->prepare($sql);
-
-						//execute the query on the server and return the result set
-						$stmt->setFetchMode(PDO::FETCH_ASSOC);
-						$stmt->execute();
-						$resultSet = $stmt->fetchAll();
-						
-						?>
-
-						<?php
-						foreach($resultSet as $row)
-						{					
-
-							echo "<a class='dropdown-item' href='#'>".$row["catname"]."</a>";		
-
-						}
-						?>	
-	      </div>
-	    </li>
-	  </ul>
-	</nav>
-
-
-	<div class="row">
-	  <div class="column side">
-	    <h2>Side</h2>
-	    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit..</p>
-	  </div>
-	  
-	  <div class="column middle">
-	  		
-	  			<?php
-					$sql = "select * from Toy";
-
-					//////////////////////////////////////
-					$stmt = $pdo->prepare($sql);
-
-					//execute the query on the server and return the result set
-					$stmt->setFetchMode(PDO::FETCH_ASSOC);
-					$stmt->execute();
-					$resultSet = $stmt->fetchAll();
-						
-				?>
-				<?php
-					foreach($resultSet as $row)
-					{	
-				?>				
-						<div class="product" >
-							<?php
-								echo "<div class='name'>".$row["toyname"] ."</div>";		
-								echo "<div class='image'><img src='" .$row["image"] ."' alt='' width='100%' height='100%'></div>";
-								echo "<div class='price'>".$row["price"] ."</div>";	
-							?>
-						</div>
-				<?php 
-					}
-				?>	
-
-			
-	</div>
-	  
-	  <div class="column side">
-	    <h2>Side</h2>
-	    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit..</p>
-	  </div>
-	</div>
-
-<div class="footer">
-  <table align="center" cellspacing="15px;">
-  		<td> 
-			<li>20 Hai Ba Trung street, Hoan Kiem distric <br>   024 36320669/ 0961205601 </li>
-					
-		</td>
-		<td>
-			<li>100 Cau Giay street, Cau Giay distric <br>   024 36320669/ 0961205601</li>
-		
-		</td>
-	</table>
-</div>
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
+				<form class="login100-form validate-form" action="home.php" method="Post">
+					<span class="login100-form-title p-b-33">
+						Login
+					</span>
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz" name="username">
+						<input class="input100" type="text" name="username" placeholder="Email">
+						<span class="focus-input100-1"></span>
+						<span class="focus-input100-2"></span>
+					</div>
+					<div class="wrap-input100 rs1 validate-input" data-validate="Password is required" name="pass">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100-1"></span>
+						<span class="focus-input100-2"></span>
+					</div>
+					<div class="container-login100-form-btn m-t-20">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
 
 </body>
 </html>
