002d130384908731e6628a342dd28c9f7ef40564
Nguyen Phuong Thuy <phuongthuya3k52@gmail.com>
2019-10-30 03:51:28 +0700

    Update home.php

--- home.php
+++ home.php
@@ -82,6 +82,6 @@ if(isset($_POST["username"]) && isset($_POST["pass"]))
 	?>
 			</table></br>
 		</div>
-		<div class="last"><button><a href="/add.php">Add</a></button></div>
+		<div class="last"><button><a href="/add.php" style="font-weight: 1px, font-size: 22px">Add</a></button></div>
 </body>
 </html>
