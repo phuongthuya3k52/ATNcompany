6801c19bf4b725ee5a03e4059e96e5c866259106
Nguyen Phuong Thuy <phuongthuya3k52@gmail.com>
2019-10-30 00:36:23 +0700

    Update dbconnector.php

--- dbconnector.php
+++ dbconnector.php
@@ -1,3 +1,3 @@
 <?php
-$conn = pg_connect("host=ec2-23-21-160-38.compute-1.amazonaws.com port = 5432 dbname= dbjfoa0gfe9evt user= jzidaowoheuhua password= fc1db7716f804b61c3182a5ff5bdeb1a34fee534a953a3d3369af51ca2b42d69") or die("unstable connection")
+$conn = pg_connect("host=ec2-174-129-18-42.compute-1.amazonaws.com  port = 5432 dbname= d4ucl7dvgperco  user= yxmokirelqewcc  password= 2e22808668584561bfbe7c454db16a062574951e2dd8cf55553d9fdf0b44787f") or die("unstable connection")
   ?>
