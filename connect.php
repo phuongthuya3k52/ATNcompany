09a11f3d2c41096977db8d4cbf90dcc7f9e3bc7c
Nguyen Phuong Thuy <phuongthuya3k52@gmail.com>
2019-10-30 00:36:19 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
   if (empty(getenv("DATABASE_URL"))){
-      $pdo = new PDO('pgsql:host=ec2-23-21-160-38.compute-1.amazonaws.com;port=5432;dbname=dbjfoa0gfe9evt', 'jzidaowoheuhua', 'fc1db7716f804b61c3182a5ff5bdeb1a34fee534a953a3d3369af51ca2b42d69');
+      $pdo = new PDO('pgsql:host=ec2-174-129-18-42.compute-1.amazonaws.com;port=5432;dbname=d4ucl7dvgperco', 'yxmokirelqewcc', '2e22808668584561bfbe7c454db16a062574951e2dd8cf55553d9fdf0b44787f');
   }  else {
     $db = parse_url(getenv("DATABASE_URL"));
     $pdo = new PDO("pgsql:" . sprintf(
