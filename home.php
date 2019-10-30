2399215cde47800b622c36ee8cfa7d5d61603b46
Nguyen Phuong Thuy <phuongthuya3k52@gmail.com>
2019-10-31 01:05:24 +0700

    Update home.php

--- home.php
+++ home.php
@@ -87,11 +87,10 @@ if(isset($_POST["username"]) && isset($_POST["pass"]))
 		        	<input type='hidden' name='idtoy' value='<?php echo $row['idtoy']?>'>
 		        	<input class="edit-btn" type='submit' value='Delete'>
 		        </form> <br><br>
-		        <a href="./edit.php?idtoy=<?=$row['idtoy']?>">Edit</a>
-		        <!-- <form action='/edit.php' method="GET">
+		        <form action='/edit.php' method="GET">
 		        	<input type='hidden' name='idto' value='<?php echo $row['idtoy']?>'>
 		        	<input class="edit-btn" type='submit' value='Edit'>
-		        </form> --> 
+		        </form> 
 		        </td>
 			</tr>
 	<?php
