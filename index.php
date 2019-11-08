4162d1d99cb399a97a95588458ee890ca9be8300
Nguyen Phuong Thuy <phuongthuya3k52@gmail.com>
2019-11-09 02:26:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -53,29 +53,6 @@
 				</form>
 			</div>
 		</div>
-		<?php  
-			require_once './dbconnector.php';
-			if(isset($_POST["username"]) && isset($_POST["pass"]))
-			{
-				$user = $_POST["username"];
-				$pass = $_POST["pass"];
-				$sql ="SELECT * FROM Account WHERE username = '$user' AND password= '$pass'";
-				$rows = pg_query($sql);
-				if(pg_num_rows($rows)==1) { ?>
-					<script>
-			            alert("Login successfully!!");
-			            window.location.href = "/home.php";
-			        </script>
-			    <?php
-			    } else { 
-			        ?>
-			            <script>
-			                alert("Wrong Username/Password");
-			                window.location.href = "/index.php";
-			            </script>
-			        <?php }
-			}
-		?>
 	</div>	
 <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
